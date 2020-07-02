c6400269436c53ea632231080affefb9923e3a5a
Raas Ahsan <raas.ahsan@gmail.com>
2020-07-02 14:49:37 -0500

    typed lambda calculus: abstraction, application and variable

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,6 +3,9 @@ object Main {
   
   // Representation of the abstract syntax tree.
   enum Term derives Eql {
+    case TmVar(name: String)
+    case TmAbs(name: String, ty: Type, t: Term)
+    case TmApp(t1: Term, t2: Term)
     case TmInt(x: Int)
     case TmAdd(t1: Term, t2: Term)
     case TmTrue
@@ -15,64 +18,100 @@ object Main {
   }
   
   enum Type derives Eql {
+    case TyFunc(ty1: Type, ty2: Type)
     case TyInt
     case TyBool
     case TyUnit
   }
   
+  opaque type Context = Map[String, Type]
+  
+  object Context {
+    def apply(): Context = Map()
+  }
+  
+  extension contextOps on (ctx: Context) {
+    def get(name: String): Option[Type] =
+      ctx.get(name)
+    
+    def add(name: String, ty: Type): Context =
+      ctx + (name -> ty)
+  }
+  
   // Each case arm captures an inference rule in the typing relation.
   // Premises are checked, and the return value reflects the conclusion.
-  def typecheck(term: Term): Either[String, Type] = term match {
+  def typecheck(term: Term, ctx: Context): Either[String, Type] = term match {
+    case Term.TmVar(name) =>
+      ctx.get(name).fold(Left(s"no type binding found for $name"))(Right.apply)
+    case Term.TmAbs(name, ty, t) => 
+      val ctx0 = ctx.add(name, ty)
+      for {
+        ty2 <- typecheck(t, ctx0) 
+      } yield Type.TyFunc(ty, ty2)
+    case Term.TmApp(t1, t2) =>
+      for {
+        ty1 <- typecheck(t1, ctx)
+        ty2 <- typecheck(t2, ctx)
+        fty <- ty1 match {
+          case Type.TyFunc(fty1, fty2) => Right((fty1, fty2)) 
+          case _ => Left(s"left hand of abstraction is not a function. $t2: $ty2")
+        }
+        _   <- if ty2 == fty._1 then Right(()) else Left(s"type mismatch: applied function of type $ty1 to a term of type $ty2")
+      } yield fty._2
     case _: Term.TmInt => Right(Type.TyInt)
     case Term.TmAdd(t1, t2) => 
       for {
-        _   <- termTypeEquals(t1, Type.TyInt)
-        _   <- termTypeEquals(t2, Type.TyInt)
+        _   <- checkTermType(t1, Type.TyInt, ctx)
+        _   <- checkTermType(t2, Type.TyInt, ctx)
       } yield Type.TyInt
     case Term.TmTrue => Right(Type.TyBool)
     case Term.TmFalse => Right(Type.TyBool)
     case Term.TmAnd(t1, t2) =>
       for {
-        _   <- termTypeEquals(t1, Type.TyBool)
-        _   <- termTypeEquals(t2, Type.TyBool)
+        _   <- checkTermType(t1, Type.TyBool, ctx)
+        _   <- checkTermType(t2, Type.TyBool, ctx)
       } yield Type.TyBool
     case Term.TmOr(t1, t2) =>
       for {
-        _   <- termTypeEquals(t1, Type.TyBool)
-        _   <- termTypeEquals(t2, Type.TyBool)
+        _   <- checkTermType(t1, Type.TyBool, ctx)
+        _   <- checkTermType(t2, Type.TyBool, ctx)
       } yield Type.TyBool
     case Term.TmNot(t1) =>
       for {
-        _   <- termTypeEquals(t1, Type.TyBool)
+        _   <- checkTermType(t1, Type.TyBool, ctx)
       } yield Type.TyBool
     case Term.TmIf(t1, t2, t3) =>
       for {
-        _   <- termTypeEquals(t1, Type.TyBool)
-        ty2 <- typecheck(t2)
-        ty3 <- typecheck(t3)
-        _   <- typesEqual(ty2, ty3)
+        _   <- checkTermType(t1, Type.TyBool, ctx)
+        ty2 <- typecheck(t2, ctx)
+        ty3 <- typecheck(t3, ctx)
+        _   <- typesMatch(ty2, ty3)
       } yield ty2
     case Term.TmUnit => Right(Type.TyUnit)
   }
   
-  def termTypeEquals(t: Term, ty: Type): Either[String, Unit] =
-    typecheck(t).flatMap(ty0 => typesEqual(ty0, ty))
+  def checkTermType(t: Term, ty: Type, ctx: Context): Either[String, Unit] =
+    typecheck(t, ctx).flatMap(ty0 => typesMatch(ty0, ty))
   
-  def typesEqual(ty1: Type, ty2: Type): Either[String, Unit] =
+  def typesMatch(ty1: Type, ty2: Type): Either[String, Unit] =
     if ty1 == ty2 
       Right(()) 
     else 
-      Left("types do not match")
+      Left(s"type mismatch: $ty1 ; $ty2")
 
   def main(args: Array[String]): Unit = {
     import Term._
+    import Type._
     val ast = TmIf(
       TmOr(TmAnd(TmTrue, TmTrue), TmFalse),
-      TmFalse,
-      TmInt(10)
+      TmInt(30),
+      TmApp(
+        TmAbs("x", TyInt, TmAdd(TmVar("x"), TmVar("x"))),
+        TmAdd(TmInt(10), TmInt(20))
+      )
     )
     
-    val res = typecheck(ast)
+    val res = typecheck(ast, Context())
     println(res)
   }
 
--- src/test/scala/TypeCheckTests.scala
+++ src/test/scala/TypeCheckTests.scala
@@ -7,87 +7,119 @@ class TypeCheckTests extends AnyFunSuite with Matchers {
   import Term._
   import Type._
   
+  val ctx = Context() // empty context
   type TypeCheckFail = Left[String, Unit]
   
   // TODO: Introduce property-based tests
   
+  test("TmVar is ill-typed with no binding") {
+    // free variables should never type with no typing context
+    val t = TmVar("x")
+    typecheck(t, ctx) shouldBe a [TypeCheckFail]
+  }
+  
+  test("TmAbs types to TyFunc") {
+    val t = TmAbs("x", TyInt, TmVar("x"))
+    typecheck(t, ctx) shouldBe Right(TyFunc(TyInt, TyInt))
+  }
+
+  test("TmAbs is ill-typed if body is ill-typed") {
+    val t = TmAbs("x", TyInt, TmVar("y"))
+    typecheck(t, ctx) shouldBe a [TypeCheckFail]
+  }
+
+  test("TmApp types to return type of left hand side abstraction") {
+    val t = TmApp(TmAbs("x", TyInt, TmVar("x")), TmInt(10))
+    typecheck(t, ctx) shouldBe Right(TyInt)
+  }
+  
+  test("TmApp is ill-typed if there is a type mismatch in application") {
+    val t = TmApp(TmAbs("x", TyInt, TmVar("x")), TmTrue)
+    typecheck(t, ctx) shouldBe a [TypeCheckFail]
+  }
+
+  test("TmApp is ill-typed if left-hand side is not an abstraction") {
+    val t = TmApp(TmTrue, TmTrue)
+    typecheck(t, ctx) shouldBe a [TypeCheckFail]
+  }
+  
   test("TmInt types to TyInt") {
     val t = TmInt(3)
-    typecheck(t) shouldBe Right(TyInt)
+    typecheck(t, ctx) shouldBe Right(TyInt)
   }
 
   test("TmAdd types if arguments have type TyInt") {
     val t = TmAdd(TmInt(10), TmInt(20))
-    typecheck(t) shouldBe Right(TyInt)
+    typecheck(t, ctx) shouldBe Right(TyInt)
   }
 
-  test("TmAdd doesn't type if left term is not of type TyInt") {
+  test("TmAdd is ill-typed if left term is not of type TyInt") {
     val t = TmAdd(TmFalse, TmInt(10))
-    typecheck(t) shouldBe a [TypeCheckFail]
+    typecheck(t, ctx) shouldBe a [TypeCheckFail]
   }
 
   test("TmAdd doesn't type if right term is not of type TyInt") {
     val t = TmAdd(TmInt(10), TmTrue)
-    typecheck(t) shouldBe a [TypeCheckFail]
+    typecheck(t, ctx) shouldBe a [TypeCheckFail]
   }
   
   test("TmTrue types to TyBool") {
     val t = TmTrue
-    typecheck(t) shouldBe Right(TyBool)
+    typecheck(t, ctx) shouldBe Right(TyBool)
   }
 
   test("TmFalse types to TyBool") {
     val t = TmFalse
-    typecheck(t) shouldBe Right(TyBool)
+    typecheck(t, ctx) shouldBe Right(TyBool)
   }
   
   test("TmAnd types if arguments have type TyBool") {
     val t = TmAnd(TmTrue, TmFalse)
-    typecheck(t) shouldBe Right(TyBool)
+    typecheck(t, ctx) shouldBe Right(TyBool)
   }
   
   test("TmAnd doesn't type if left term is not of type TyBool") {
     val t = TmAnd(TmInt(10), TmFalse)
-    typecheck(t) shouldBe a [TypeCheckFail]
+    typecheck(t, ctx) shouldBe a [TypeCheckFail]
   }
   
   test("TmAnd doesn't type if right term is not of type TyBool") {
     val t = TmAnd(TmTrue, TmInt(10))
-    typecheck(t) shouldBe a [TypeCheckFail]
+    typecheck(t, ctx) shouldBe a [TypeCheckFail]
   }
 
   test("TmOr types if arguments have type TyBool") {
     val t = TmOr(TmTrue, TmFalse)
-    typecheck(t) shouldBe Right(TyBool)
+    typecheck(t, ctx) shouldBe Right(TyBool)
   }
 
   test("TmOr doesn't type if left term is not of type TyBool") {
     val t = TmOr(TmInt(10), TmFalse)
-    typecheck(t) shouldBe a [TypeCheckFail]
+    typecheck(t, ctx) shouldBe a [TypeCheckFail]
   }
 
   test("TmOr doesn't type if right term is not of type TyBool") {
     val t = TmOr(TmTrue, TmInt(10))
-    typecheck(t) shouldBe a [TypeCheckFail]
+    typecheck(t, ctx) shouldBe a [TypeCheckFail]
   }
   
   test("TmIf types if guard has TyBool and branch types are equal") {
     val t = TmIf(TmTrue, TmInt(10), TmInt(20))
-    typecheck(t) shouldBe Right(TyInt)
+    typecheck(t, ctx) shouldBe Right(TyInt)
   }
 
   test("TmIf doesn't type if guard is not of type TyBool") {
     val t = TmIf(TmInt(10), TmInt(10), TmInt(20))
-    typecheck(t) shouldBe a [TypeCheckFail]
+    typecheck(t, ctx) shouldBe a [TypeCheckFail]
   }
 
   test("TmIf doesn't type if branch types are not equal") {
     val t = TmIf(TmTrue, TmFalse, TmInt(20))
-    typecheck(t) shouldBe a [TypeCheckFail]
+    typecheck(t, ctx) shouldBe a [TypeCheckFail]
   }
   
   test("TmUnit types to TyUnit") {
     val t = TmUnit
-    typecheck(t) shouldBe Right(TyUnit)
+    typecheck(t, ctx) shouldBe Right(TyUnit)
   }
 }
